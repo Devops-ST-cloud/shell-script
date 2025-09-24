4c7c71877e4759f88d3cf7307d8a4665521a4414
Devops-ST-cloud <boyasuryateja77@gmail.com>
2025-09-24 15:07:46 +0530

    new

--- 06-variables.sh
+++ 06-variables.sh
@@ -5,6 +5,6 @@ movies=("DON","RRR","OG")
 # size of array is 3
 # indexes of array is 0,1,2
 
-echo "Movie Name::${movies[0]}"
-echo "Movie Name::${movies[1]}"
-echo "Movie Name::${movies[2]}"
+echo "First Movie Name::${movies[0]}"
+echo "Second Movie Name::${movies[1]}"
+echo "Third Movie Name::${movies[2]}"
