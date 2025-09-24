9bacd05f50a37dd8c37d376e9201f5b5bb6b2d6a
Devops-ST-cloud <boyasuryateja77@gmail.com>
2025-09-24 14:12:04 +0530

    new

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi $PERSON1 How are you"
+echo "$PERSON1:: Im fine how is your work"
+echo "$PERSON2:: My Work was Fantastic"
