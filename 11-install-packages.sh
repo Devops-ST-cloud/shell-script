510c3b8080f7c9ee6ca03f41ed0de3c264ca06b2
Devops-ST-cloud <boyasuryateja77@gmail.com>
2025-09-25 23:48:14 +0530

    functions

--- 11-install-packages.sh
+++ 11-install-packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 if [ $USERID -ne 0 ]
   then 
       echo "please run the script as root user"
@@ -12,4 +16,11 @@ fi
 for i in $@
 do 
    echo "package install:: $i"
+   dnf list installed $i &>>$LOGFILE
+   if [ $? -eq 0 ]
+   then
+       echo "$i already installed"
+    else
+        echo "$i not installed ... need to install"
+    fi       
 done   
