8d3661825ccbf2a5f8bcb9156c25fea6af34bf49
Devops-ST-cloud <boyasuryateja77@gmail.com>
2025-09-25 22:43:22 +0530

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$0/$TIMESTAMP-$SCRIPT_NAME.log
 
 VALIDATE(){
